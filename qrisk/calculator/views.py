6a45230f6e8a95222c5c26a6b95ab47d62e097b2
andy <andy.idiaghe@voyagecontrol.com>
2016-09-13 17:45:01 +0100

    ENH: Modifying api resp/res

--- qrisk/calculator/views.py
+++ qrisk/calculator/views.py
@@ -1,5 +1,9 @@
-from qrisk.calculator.models import QUserInfo
+from qrisk.calculator.models import QUserInfo, QUser
 from qrisk.calculator.serializers import QInfoSerializer
+from qrisk.calculator.utils import calculate
+
+
+from django.db import transaction
 
 from rest_framework import generics
 
@@ -10,5 +14,13 @@ class QInfoDetail(generics.ListCreateAPIView):
     queryset = QUserInfo.objects.all()
 
     def post(self, request, *args, **kwargs):
-        import pdb; pdb.set_trace()
-        return super(QInfoDetail, self).post(request, args, kwargs)
+
+        with transaction.atomic():
+            resp = super(QInfoDetail, self).post(request, args, kwargs)
+            quser = QUser.objects.last()
+            # Calculate risk and then create object or viceversa?
+            if resp.status_code == 201:
+                result = calculate(quser)
+
+        return result
+
